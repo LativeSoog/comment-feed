73933edf6b34a94aa018a45b3551cc51b2fdccde
Vitaliy <konfet88@gmail.com>
2023-05-02 00:42:57 +0600

    - Added script module - Added module "API" (get, post)

---
+++ api.js
@@ -0,0 +1,29 @@
+//API GET
+const apiFetchGet = () => {
+    return fetch("https://webdev-hw-api.vercel.app/api/v1/vitaliy-gusev/comments", {
+        method: "GET"
+    })
+}
+
+//API POST
+const apiFetchPost = (inputName, textComment, getDate) => {
+    return fetch("https://webdev-hw-api.vercel.app/api/v1/vitaliy-gusev/comments", {
+        method: "POST",
+        body: JSON.stringify({
+            name: inputName
+                .replaceAll("<", "&lt;")
+                .replaceAll(">", "&gt;")
+                .replaceAll("/", "&frasl;"),
+            date: getDate(),
+            text: textComment
+                .replaceAll("<", "&lt;")
+                .replaceAll(">", "&gt;")
+                .replaceAll("/", "&frasl;")
+                .replaceAll("QUOTE_START", '<div class="quote">')
+                .replaceAll("QUOTE_END", '</div>'),
+            forceError: true,
+        })
+    })
+}
+
+export { apiFetchGet, apiFetchPost }
